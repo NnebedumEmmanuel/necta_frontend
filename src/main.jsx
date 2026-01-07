d71b4900c80ae2333ec8144da13cef3a6ee37803
Nnebedum_emmanuel <97546469+NnebedumEmmanuel@users.noreply.github.com>
2026-01-07 08:51:21 -0500

    fix(providers): add ToastProvider around Cart/Wishlist so useToast is available

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { WishlistProvider } from '../context/WishlistContext';
-import { CartProvider } from '../context/CartContext';
-
+import { WishlistProvider } from '../../context/WishlistContext';
+import { CartProvider } from '../../context/CartContext';
+import ToastProvider from './context/ToastProvider';
 import { AuthProvider } from '@/context/AuthContext';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <CartProvider>
-        <WishlistProvider>
-          <App />
-        </WishlistProvider>
-      </CartProvider>
+      <ToastProvider>
+        <CartProvider>
+          <WishlistProvider>
+            <App />
+          </WishlistProvider>
+        </CartProvider>
+      </ToastProvider>
     </AuthProvider>
   </React.StrictMode>
 );
